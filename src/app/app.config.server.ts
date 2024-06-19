c365712d9f2b3fdba3bda9d6d5ed0cd366cfe868
Daniel Kimaiga <68325982+pwejar@users.noreply.github.com>
2024-06-19 08:20:37 +0300

    app config corrected

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,18 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { routes } from './app.routes';
+// import { appConfig } from './app.config';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
   ]
 };
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+// export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = serverConfig;
